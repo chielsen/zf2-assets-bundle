011b0c98805b4a9e255209b6ce17a7d460d971f1
Michiel Thalen <michiel.thalen@ccgroup.nl>
2018-12-21 23:02:50 +0100

    use new minfiers

--- src/AssetsBundle/AssetFile/AssetFileFilter/CssAssetFileFilter.php
+++ src/AssetsBundle/AssetFile/AssetFileFilter/CssAssetFileFilter.php
@@ -2,46 +2,48 @@
 
 namespace AssetsBundle\AssetFile\AssetFileFilter;
 
+use tubalmartin\CssMin\Minifier as CSSmin;
+
 class CssAssetFileFilter extends \AssetsBundle\AssetFile\AssetFileFilter\AbstractAssetFileFilter {
 
-    /**
-     * @var string
-     */
-    protected $assetFileFilterName = \AssetsBundle\AssetFile\AssetFile::ASSET_CSS;
+	/**
+	 * @var string
+	 */
+	protected $assetFileFilterName = \AssetsBundle\AssetFile\AssetFile::ASSET_CSS;
 
-    /**
-     * @var \CSSmin
-     */
-    protected $cssMin;
+	/**
+	 * @var CSSmin
+	 */
+	protected $cssMin;
 
-    /**
-     * @param \AssetsBundle\AssetFile\AssetFile $oAssetFile
-     * @throws \LogicException
-     * @throws \InvalidArgumentException
-     * @return string
-     */
-    public function filterAssetFile(\AssetsBundle\AssetFile\AssetFile $oAssetFile) {
-        //Try to retrieve cached filter rendering
-        if ($sCachedFilterRendering = $this->getCachedFilteredContent($oAssetFile)) {
-            return $sCachedFilterRendering;
-        }
-        $sFilteredContent = $this->getCSSmin()->run($oAssetFile->getAssetFileContents());
-        $this->cacheFilteredAssetFileContent($oAssetFile, $sFilteredContent);
-        return $sFilteredContent;
-    }
+	/**
+	 * @param \AssetsBundle\AssetFile\AssetFile $oAssetFile
+	 * @throws \LogicException
+	 * @throws \InvalidArgumentException
+	 * @return string
+	 */
+	public function filterAssetFile(\AssetsBundle\AssetFile\AssetFile $oAssetFile) {
+		//Try to retrieve cached filter rendering
+		if ($sCachedFilterRendering = $this->getCachedFilteredContent($oAssetFile)) {
+			return $sCachedFilterRendering;
+		}
+		$sFilteredContent = $this->getCSSmin()->run($oAssetFile->getAssetFileContents());
+		$this->cacheFilteredAssetFileContent($oAssetFile, $sFilteredContent);
+		return $sFilteredContent;
+	}
 
-    /**
-     * @return \CSSmin
-     * @throws \LogicException
-     */
-    protected function getCSSmin() {
-        if ($this->cssMin instanceof \CSSmin) {
-            return $this->cssMin;
-        }
-        if (!class_exists('CSSmin')) {
-            throw new \LogicException('"CSSmin" class does not exist');
-        }
-        return $this->cssMin = new \CSSmin(false);
-    }
+	/**
+	 * @return CSSmin
+	 * @throws \LogicException
+	 */
+	protected function getCSSmin() {
+		if ($this->cssMin instanceof CSSmin) {
+			return $this->cssMin;
+		}
+//        if (!class_exists('CSSmin')) {
+//            throw new \LogicException('"CSSmin" class does not exist');
+//        }
+		return $this->cssMin = new CSSmin(false);
+	}
 
 }
--- src/AssetsBundle/AssetFile/AssetFileFilter/JsAssetFileFilter/JsMinAssetFileFilter.php
+++ src/AssetsBundle/AssetFile/AssetFileFilter/JsAssetFileFilter/JsMinAssetFileFilter.php
@@ -2,24 +2,26 @@
 
 namespace AssetsBundle\AssetFile\AssetFileFilter\JsAssetFileFilter;
 
+use JSMin\JSMin;
+
 class JsMinAssetFileFilter extends \AssetsBundle\AssetFile\AssetFileFilter\JsAssetFileFilter\AbstractJsAssetFileFilter
 {
-        
-    /**
-     * @var string $sContent
-     * @return string
-     * @throws \LogicException
-     * @throws \InvalidArgumentException
-     */
-    protected function minifyJsContent($sContent)
-    {
-        if (!is_string($sContent)) {
-            throw new \InvalidArgumentException('Argument "$sContent" expects a string, "'.(is_object($sContent)?get_class($sContent):gettype($sContent)).'" given');
-        }
-        if (!class_exists('JSMin')) {
-            throw new \LogicException('"JSMin" class does not exist');
-        }
-        return  \JSMin::minify($sContent);
-    }
+
+	/**
+	 * @var string $sContent
+	 * @return string
+	 * @throws \LogicException
+	 * @throws \InvalidArgumentException
+	 */
+	protected function minifyJsContent($sContent)
+	{
+		if (!is_string($sContent)) {
+			throw new \InvalidArgumentException('Argument "$sContent" expects a string, "'.(is_object($sContent)?get_class($sContent):gettype($sContent)).'" given');
+		}
+//        if (!class_exists('JSMin')) {
+//            throw new \LogicException('"JSMin" class does not exist');
+//        }
+		return  JSMin::minify($sContent);
+	}
 
 }
